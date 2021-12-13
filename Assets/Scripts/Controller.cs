97f89c85fe577abb527529ffa2a82f4768e4778d
Daniel Williams <danieljameswillliams2016@gmail.com>
2021-12-13 07:16:24 +0000

    Added ControllerManager

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -2,20 +2,30 @@
 
 public class Controller : MonoBehaviour
 {
-    public int Index;// { get; private set; }
+    public int Index { get; private set; }
+    public bool IsAssigned { get; set; }
 
     private string attackButton;
 
     public bool attack;
-    
-    private void Start()
-    {
-        //Index = 1;
-        attackButton = "Attack" + Index;
-    }
 
     private void Update()
     {
-        attack = Input.GetButton(attackButton);
+        if (!string.IsNullOrEmpty(attackButton))
+        {
+            attack = Input.GetButton(attackButton);
+        }
+    }
+
+    public void SetIndex(int index)
+    {
+        Index = index;
+        attackButton = "Attack" + Index;
+        gameObject.name = "Controller" + Index;
+    }
+
+    public bool AnyButtonDown()
+    {
+        return attack;
     }
 }

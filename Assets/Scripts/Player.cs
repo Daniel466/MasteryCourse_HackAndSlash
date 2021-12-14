17121e18df02f5f3b958be68020b5a0cb5a152dc
DESKTOP-204F2VG\Daniel466 <danieljameswillliams2016@gmail.com>
2021-12-14 16:45:34 +0000

    Setup Players & Controllers

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,27 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private int playerNumber;
     
     private Controller controller;
-    
+    private UIPlayerText uiPlayerText;
+
     public bool HasController { get { return controller != null; } }
-    public int PlayerNumber { get { return  playerNumber; } }
+    public int PlayerNumber { get { return playerNumber; } }
+
+    private void Awake()
+    {
+        uiPlayerText = GetComponentInChildren<UIPlayerText>();
+    }
 
     public void InitializePlayer(Controller controller)
     {
         this.controller = controller;
 
         gameObject.name = string.Format("Player {0} - {1}", playerNumber, controller.gameObject.name);
+
+        uiPlayerText.HandlePlayerInitialized();
     }
 }
---
+++ Assets/Scripts/UI/UIPlayerText.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class UIPlayerText : MonoBehaviour
+{
+    private TextMeshProUGUI tmText;
+
+    private void Awake()
+    {
+        tmText = GetComponent<TextMeshProUGUI>();
+    }
+
+    internal void HandlePlayerInitialized()
+    {
+        tmText.text = "Player Joined";
+        StartCoroutine(ClearTextAfterDelay());
+    }
+
+    private IEnumerator ClearTextAfterDelay()
+    {
+        yield return new WaitForSeconds(2);
+        tmText.text = String.Empty;
+    }
+}

ce33f113525d81980073c07de541d642a17e81d7
Daniel Williams <danieljameswillliams2016@gmail.com>
2021-12-21 06:09:48 +0000

    Wizard Walk and Run Animations

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 public class Character : MonoBehaviour
 {
     private Controller controller;
+    private Animator animator;
     
     [SerializeField] private float moveSpeed = 5f;
+
+    private void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
 
     internal void SetController(Controller controller)
     {
@@ -19,6 +25,12 @@ public class Character : MonoBehaviour
         {
             transform.position += direction * Time.deltaTime * moveSpeed;
             transform.forward = direction * 360f;
+            
+            animator.SetFloat("Speed", direction.magnitude);
+        }
+        else
+        {
+            animator.SetFloat("Speed", 0);
         }
     }
 }

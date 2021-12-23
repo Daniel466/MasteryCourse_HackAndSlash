17bf62de2fc22f1f18ac81e941f7761afb44f7b1
DESKTOP-204F2VG\Daniel466 <danieljameswillliams2016@gmail.com>
2021-12-23 20:33:53 +0000

    PlayerTracker

---
+++ Assets/Scripts/PlayerTracker.cs
@@ -0,0 +1,25 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+public class PlayerTracker : MonoBehaviour
+{
+    private CinemachineTargetGroup targetGroup;
+
+    private void Awake()
+    {
+        targetGroup = GetComponent<CinemachineTargetGroup>();
+
+        var players = FindObjectsOfType<Player>();
+
+        foreach (var player in players)
+        {
+            player.OnCharacterChanged += (character) => Player_OnCharacterChanged(player, character);
+        }
+    }
+
+    private void Player_OnCharacterChanged(Player player, Character character)
+    {
+        var playerIndex = player.PlayerNumber - 1;
+        targetGroup.m_Targets[playerIndex].target = character.transform;
+    }
+}

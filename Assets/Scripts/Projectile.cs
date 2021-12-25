1649c589987c7ece10d8b97671a9fdc2ad501e52
Daniel Williams <danieljameswillliams2016@gmail.com>
2021-12-25 05:24:33 +0000

    Projectile Script

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Projectile : PooledMonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 10f;
+    
+    private void Update()
+    {
+        transform.position += transform.forward * moveSpeed * Time.deltaTime;
+    }
+}
--- Assets/Scripts/ProjectileAttacker.cs
+++ Assets/Scripts/ProjectileAttacker.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class ProjectileAttacker : MonoBehaviour, IAttack
 {
     [SerializeField] private float attackRefreshSpeed = 1f;
-    [SerializeField] private GameObject projectilePrefab;
+    [SerializeField] private Projectile projectilePrefab;
     
     private float attackTimer;
     private Animator animator;
@@ -26,6 +26,6 @@ public class ProjectileAttacker : MonoBehaviour, IAttack
     {
         animator.SetTrigger("Attack");
 
-        var projectile = Instantiate(projectilePrefab, transform.position, transform.rotation);
+        projectilePrefab.Get<Projectile>(transform.position, transform.rotation);
     }
 }

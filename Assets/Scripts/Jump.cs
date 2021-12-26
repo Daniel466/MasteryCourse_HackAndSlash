7dbf581c99a2a5366edbdac1d008f3d658c4cdcd
Daniel Williams <danieljameswillliams2016@gmail.com>
2021-12-26 03:51:04 +0000

    Jump

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Jump : AbilityBase
+{
+    [SerializeField] private float jumpForce = 100f;
+    
+    private new Rigidbody rigidbody;
+    
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    protected override void OnTryUse()
+    {
+        rigidbody.AddForce(Vector3.up * jumpForce);
+    }
+}
